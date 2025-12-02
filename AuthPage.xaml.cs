5696f01d5a7fad6e5f76dfd5b4b724a3caa75c17
hexedupvvitch <dustvast@gmail.com>
2025-12-03 02:54:27 +0300

    laba13

---
+++ AuthPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ишбулатов41размер
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Guest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TBoxpass_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TBoxlog_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuestButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RefreshCaptchaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
